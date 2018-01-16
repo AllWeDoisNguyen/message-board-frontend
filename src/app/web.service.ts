0de44f998583929f956051fda3a6b3277e0c89b2
Christina Nguyen <cnguyen2@sapient.com>
2018-01-16 15:49:59 -0500

    changing url to be the new express api router

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -8,6 +8,6 @@ export class WebService {
   constructor(private http: Http) {} // inject http into class constructor to use it and to save as a local reference for GetMessages
 
   getMessages() {
-    return this.http.get('http://localhost:1234/api/messages').toPromise();
+    return this.http.get('http://localhost:63145/api/messages').toPromise();
   }
 }
